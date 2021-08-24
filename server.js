951286c9a13c0540186e2145ea6d258ecfb6c526
Allison Harden <harden.allison@gmail.com>
2021-08-24 17:25:12 -0600

    Added login/logout functionality.

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -14,7 +27,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
